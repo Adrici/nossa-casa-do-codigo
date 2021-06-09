3dba9b0aac29c91a2a20ca34f1f920905268cf23
Adrici <cirellia5@gmail.com>
2021-06-09 10:25:38 -0300

    Criando cadastro com kotlin e micronaut - Validando Anotações com Micronaut

--- nossa-casa-do-codigo/src/main/kotlin/autor/CadasraAutorController.kt
+++ nossa-casa-do-codigo/src/main/kotlin/autor/CadasraAutorController.kt
@@ -3,12 +3,15 @@ package autor
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
+import io.micronaut.validation.Validated
+import javax.validation.Valid
 
+@Validated
 @Controller("/autores")
 class CadasraAutorController {
 
     @Post
-    fun cadastra (@Body request: NovoAutorRequest){
+    fun cadastra (@Body @Valid request: NovoAutorRequest){
         println(request)
     }
 
--- nossa-casa-do-codigo/src/main/kotlin/autor/NovoAutorRequest.kt
+++ nossa-casa-do-codigo/src/main/kotlin/autor/NovoAutorRequest.kt
@@ -1,3 +1,13 @@
 package autor
 
-class NovoAutorRequest (val nome: String, val email:String, val descricao: String)
+import io.micronaut.core.annotation.Introspected
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
+
+@Introspected
+data class NovoAutorRequest ( @field:NotBlank val nome: String,
+                              @field:NotBlank @field:Email val email:String,
+                              @field:NotBlank @field:Size(max=400) val descricao: String)
+
+//o @field: é para informar que a validação está ocorrendo em cima do atributo em questão
